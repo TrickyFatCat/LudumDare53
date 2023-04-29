20aeeea68aa43e41e2afbb951c4d0399a54ceda9
Alexander Getmansky <alex@getsky.games>
2023-04-29 17:01:04 +0300

    refactor: change LivesComponent to HitPointsComponent

--- Source/LudumDare53/Enemies/EnemyCharacter.cpp
+++ Source/LudumDare53/Enemies/EnemyCharacter.cpp
@@ -6,6 +6,7 @@
 #include "EnemyController.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "LudumDare53/Components/HitPointsComponent.h"
 #include "LudumDare53/Components/LivesComponent.h"
 
 AEnemyCharacter::AEnemyCharacter()
@@ -14,7 +15,7 @@ AEnemyCharacter::AEnemyCharacter()
 	AutoPossessAI = EAutoPossessAI::Disabled;
 	AIControllerClass = AEnemyController::StaticClass();
 
-	LivesComponent = CreateDefaultSubobject<ULivesComponent>("Lives");
+	HitPointComponent = CreateDefaultSubobject<UHitPointsComponent>("HP");
 
 	if (GetCharacterMovement())
 	{
