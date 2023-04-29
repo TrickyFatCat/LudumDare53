64783087ae163a9f82828a2c7e269340e93cbd4a
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-29 12:10:52 +0300

    Create player character

---
+++ Source/LudumDare53/Player/PlayerCharacter.cpp
@@ -0,0 +1,59 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "PlayerCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "LudumDare53/Components/HitPointsComponent.h"
+#include "LudumDare53/Components/LivesComponent.h"
+#include "LudumDare53/Components/MeatCounterComponent.h"
+
+
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm);
+
+	HitPoints = CreateDefaultSubobject<UHitPointsComponent>("HitPoints");
+	LivesComponent = CreateDefaultSubobject<ULivesComponent>("Lives");
+	MeatCounterComponent = CreateDefaultSubobject<UMeatCounterComponent>("MeatCounter");
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HitPoints->OnValueZero.AddDynamic(this, &APlayerCharacter::DecreaseLives);
+	MeatCounterComponent->OnValueIncreased.AddDynamic(this, &APlayerCharacter::HandleMeatCounterIncrease);
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void APlayerCharacter::DecreaseLives()
+{
+	LivesComponent->DecreaseValue(1);
+}
+
+void APlayerCharacter::HandleMeatCounterIncrease(const int32 NewValue, const int32 Amount)
+{
+	if (HitPoints->GetValue() >= HitPoints->GetMaxValue())
+	{
+		return;
+	}
+
+	HitPoints->IncreaseValue(1);
+}
---
+++ Source/LudumDare53/Player/PlayerCharacter.h
@@ -0,0 +1,53 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;	
+class UHitPointsComponent;
+class ULivesComponent;
+class UMeatCounterComponent;
+
+UCLASS()
+class LUDUMDARE53_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<USpringArmComponent> SpringArm = nullptr;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<UCameraComponent> Camera = nullptr;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<UHitPointsComponent> HitPoints = nullptr;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<ULivesComponent> LivesComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<UMeatCounterComponent> MeatCounterComponent = nullptr;
+
+	UFUNCTION()
+	void DecreaseLives();
+
+	UFUNCTION()
+	void HandleMeatCounterIncrease(const int32 NewValue, const int32 Amount);
+};
+
