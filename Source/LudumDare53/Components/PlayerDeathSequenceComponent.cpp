c0561eaa9a82867b92f1a58f6c81e62414c605fb
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-29 18:29:57 +0300

    Add restart functionality

--- Source/LudumDare53/Components/PlayerDeathSequenceComponent.cpp
+++ Source/LudumDare53/Components/PlayerDeathSequenceComponent.cpp
@@ -3,6 +3,9 @@
 
 #include "PlayerDeathSequenceComponent.h"
 
+#include "TransitionScreenWidget.h"
+#include "Blueprint/UserWidget.h"
+#include "GameFramework/Character.h"
 
 UPlayerDeathSequenceComponent::UPlayerDeathSequenceComponent()
 {
@@ -10,12 +13,51 @@ UPlayerDeathSequenceComponent::UPlayerDeathSequenceComponent()
 	bWantsInitializeComponent = true;
 }
 
+
 void UPlayerDeathSequenceComponent::InitializeComponent()
 {
 	Super::InitializeComponent();
 }
 
+
+void UPlayerDeathSequenceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialTransform = GetOwner()->GetActorTransform();
+	
+	if (TransitionWidgetClass)
+	{
+		TransitionScreenWidget = CreateWidget<UTransitionScreenWidget>(GetWorld(), TransitionWidgetClass);
+		TransitionScreenWidget->AddToViewport();
+		TransitionScreenWidget->SetVisibility(ESlateVisibility::Hidden);
+		TransitionScreenWidget->OnShowed.AddDynamic(this, &UPlayerDeathSequenceComponent::FinishRestart);
+	}
+}
+
 void UPlayerDeathSequenceComponent::HandleDeathSequenceFinish()
 {
 	Super::HandleDeathSequenceFinish();
+	
+	if (!TransitionScreenWidget || bIsGameOver)
+	{
+		return;
+	}
+
+	if (bIsGameOver)
+	{
+		return;
+	}
+	
+	TransitionScreenWidget->SetVisibility(ESlateVisibility::HitTestInvisible);
+	TransitionScreenWidget->Show();
+}
+
+
+void UPlayerDeathSequenceComponent::FinishRestart()
+{
+	GetOwner()->SetActorTransform(InitialTransform);
+	Character->GetMesh()->bPauseAnims = false;
+	OnRespawnFinished.Broadcast();
+	TransitionScreenWidget->Hide();
 }
--- Source/LudumDare53/Components/PlayerDeathSequenceComponent.h
+++ Source/LudumDare53/Components/PlayerDeathSequenceComponent.h
@@ -6,6 +6,9 @@
 #include "DeathSequenceComponent.h"
 #include "PlayerDeathSequenceComponent.generated.h"
 
+class UTransitionScreenWidget;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnRespawnFinishedSignature);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class LUDUMDARE53_API UPlayerDeathSequenceComponent : public UDeathSequenceComponent
@@ -15,9 +18,32 @@ class LUDUMDARE53_API UPlayerDeathSequenceComponent : public UDeathSequenceCompo
 public:
 	UPlayerDeathSequenceComponent();
 
+	UPROPERTY(BlueprintAssignable)
+	FOnRespawnFinishedSignature OnRespawnFinished;
+
+	bool GetIsGameOver() const { return bIsGameOver; }
+
+	void SetIsGameOver(const bool Value) { bIsGameOver = Value; }
+
 protected:
 	virtual void InitializeComponent() override;
 
+	virtual void BeginPlay() override;
+
 	virtual void HandleDeathSequenceFinish() override;
 
+	UPROPERTY(EditDefaultsOnly, Category="Character")
+	TSubclassOf<UTransitionScreenWidget> TransitionWidgetClass = nullptr;
+
+	UPROPERTY()
+	UTransitionScreenWidget* TransitionScreenWidget = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsGameOver = false;
+
+private:
+	FTransform InitialTransform;
+
+	UFUNCTION()
+	void FinishRestart();
 };
