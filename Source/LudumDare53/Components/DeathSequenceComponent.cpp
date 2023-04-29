6583924b94dbd13d406d3ced84f4f57f0e7c19ff
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-29 17:23:38 +0300

    Create basic death sequence component

---
+++ Source/LudumDare53/Components/DeathSequenceComponent.cpp
@@ -0,0 +1,62 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "DeathSequenceComponent.h"
+
+#include "GameFramework/Character.h"
+
+
+UDeathSequenceComponent::UDeathSequenceComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+
+void UDeathSequenceComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	Character = Cast<ACharacter>(GetOwner());
+
+	if (Character)
+	{
+		AnimInstance = Character->GetMesh()->GetAnimInstance();
+	
+		if (AnimInstance)
+		{
+			AnimInstance->OnMontageBlendingOut.AddDynamic(this, &UDeathSequenceComponent::FinishDeathSequence);
+		}
+	}
+}
+
+void UDeathSequenceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UDeathSequenceComponent::StartDeathSequence()
+{
+	if (!DeathAnimation || !IsValid(Character))
+	{
+		return;
+	}
+
+	Character->PlayAnimMontage(DeathAnimation);
+	OnDeathSequenceStarted.Broadcast();
+}
+
+void UDeathSequenceComponent::FinishDeathSequence(UAnimMontage* AnimMontage, const bool bIsInterrupted)
+{
+	if (AnimMontage != DeathAnimation)
+	{
+		return;
+	}
+
+	HandleDeathSequenceFinish();
+	OnDeathSequenceFinished.Broadcast();
+}
+
+void UDeathSequenceComponent::HandleDeathSequenceFinish()
+{
+}
