759a6c183fc3f126eb0a738d4e51103ff7603450
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-29 11:43:01 +0300

    Create meat counter component

---
+++ Source/LudumDare53/Components/MeatCounterComponent.cpp
@@ -0,0 +1,10 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "MeatCounterComponent.h"
+
+
+UMeatCounterComponent::UMeatCounterComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
