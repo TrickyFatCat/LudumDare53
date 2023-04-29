935a8c2562614b2e848301ec0922414605624780
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-29 17:50:16 +0300

    Create player death sequence component

---
+++ Source/LudumDare53/Components/PlayerDeathSequenceComponent.cpp
@@ -0,0 +1,21 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "PlayerDeathSequenceComponent.h"
+
+
+UPlayerDeathSequenceComponent::UPlayerDeathSequenceComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UPlayerDeathSequenceComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void UPlayerDeathSequenceComponent::HandleDeathSequenceFinish()
+{
+	Super::HandleDeathSequenceFinish();
+}
---
+++ Source/LudumDare53/Components/PlayerDeathSequenceComponent.h
@@ -0,0 +1,23 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DeathSequenceComponent.h"
+#include "PlayerDeathSequenceComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE53_API UPlayerDeathSequenceComponent : public UDeathSequenceComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerDeathSequenceComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+	virtual void HandleDeathSequenceFinish() override;
+
+};
