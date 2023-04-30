e97107df0e30dcc97a60f1a7dbdd1a953617a7a3
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-30 21:03:37 +0300

    Create super meat pickup effect

---
+++ Source/LudumDare53/Pickups/SuperMeatPickupEffect.cpp
@@ -0,0 +1,24 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "SuperMeatPickupEffect.h"
+
+#include "LudumDare53/Components/MeatCounterComponent.h"
+
+bool USuperMeatPickupEffect::ActivateEffect_Implementation(AActor* OtherActor)
+{
+	if (!IsValid(OtherActor))
+	{
+		return false;
+	}
+
+	UMeatCounterComponent* MeatCounter = OtherActor->FindComponentByClass<UMeatCounterComponent>();
+
+	if (!MeatCounter)
+	{
+		return false;
+	}
+
+	const int32 Amount = MeatCounter->GetMaxValue() - MeatCounter->GetValue() + 3;
+	return MeatCounter->IncreaseValue(Amount, false);
+}
---
+++ Source/LudumDare53/Pickups/SuperMeatPickupEffect.h
@@ -0,0 +1,19 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupEffectType.h"
+#include "SuperMeatPickupEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE53_API USuperMeatPickupEffect : public UPickupEffectType
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool ActivateEffect_Implementation(AActor* OtherActor) override;
+};
