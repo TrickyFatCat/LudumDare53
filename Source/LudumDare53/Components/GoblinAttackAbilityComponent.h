205f645c4ac69dd31782f8306924198fce72111e
Alexander Getmansky <alex@getsky.games>
2023-04-30 16:47:15 +0300

    feat (enemy): added goblin attack

---
+++ Source/LudumDare53/Components/GoblinAttackAbilityComponent.h
@@ -0,0 +1,14 @@
+// Made by TitledGooseTeam during Ludum Dare 53
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilityComponent.h"
+#include "GoblinAttackAbilityComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE53_API UGoblinAttackAbilityComponent : public UAbilityComponent
+{
+	GENERATED_BODY()
+	
+};
