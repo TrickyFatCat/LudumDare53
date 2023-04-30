c63dce77db842ae4124bf00ea356bec3cfbd6e06
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-30 20:25:27 +0300

    Create start counter component

---
+++ Source/LudumDare53/Components/StarsCounterComponent.cpp
@@ -0,0 +1,10 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "StarsCounterComponent.h"
+
+
+UStarsCounterComponent::UStarsCounterComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
