c24e9cb5f774c6341e49b687a8d98af8c9b28959
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-01 19:43:33 +0300

    Create teleportation widget

---
+++ Source/LudumDare53/Components/LevelTeleportComponent.cpp
@@ -0,0 +1,54 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+
+#include "LevelTeleportComponent.h"
+
+#include "GameModeSession.h"
+#include "TransitionScreenWidget.h"
+#include "TrickyGameModeLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ULevelTeleportComponent::ULevelTeleportComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void ULevelTeleportComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (TransitionWidgetClass)
+	{
+		TransitionScreenWidget = CreateWidget<UTransitionScreenWidget>(GetWorld(), TransitionWidgetClass);
+		TransitionScreenWidget->AddToViewport();
+		TransitionScreenWidget->SetVisibility(ESlateVisibility::Hidden);
+		TransitionScreenWidget->OnShowed.AddDynamic(this, &ULevelTeleportComponent::FinishTeleportation);
+	}
+}
+
+void ULevelTeleportComponent::FinishTeleportation()
+{
+	UGameplayStatics::OpenLevel(this, TargetLevelName);
+}
+
+void ULevelTeleportComponent::StartTeleportation()
+{
+	OnTeleportationStarted.Broadcast();
+
+	AGameModeSession* GameModeSession = UTrickyGameModeLibrary::GetSessionGameMode(this);
+
+	if (GameModeSession)
+	{
+		GameModeSession->FinishSession(true);
+	}
+	
+	if (!TransitionScreenWidget)
+	{
+		return;
+	}
+
+	TransitionScreenWidget->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+	TransitionScreenWidget->Show();
+}
---
+++ Source/LudumDare53/Components/LevelTeleportComponent.h
@@ -0,0 +1,41 @@
+﻿// Made by TitledGooseTeam during Ludum Dare 53
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LevelTeleportComponent.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnTeleportationStartedSignatue);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE53_API ULevelTeleportComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	ULevelTeleportComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName TargetLevelName = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UTransitionScreenWidget> TransitionWidgetClass = nullptr;
+
+	UPROPERTY()
+	UTransitionScreenWidget* TransitionScreenWidget = nullptr;
+
+	UFUNCTION()
+	void FinishTeleportation();
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnTeleportationStartedSignatue OnTeleportationStarted;
+
+	UFUNCTION(BlueprintCallable)
+	void StartTeleportation();
+};
