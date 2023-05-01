db099ec00a1cefe54ae4e10ee0319ff2560d4c1a
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-01 15:56:18 +0300

    Implement game over screen

--- Plugins/TrickyUserInterface/Source/TrickyUserInterface/Public/Gameplay/FinishMenuWidget.h
+++ Plugins/TrickyUserInterface/Source/TrickyUserInterface/Public/Gameplay/FinishMenuWidget.h
@@ -29,7 +29,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="FinishScreen", meta=(BindWidget))
 	TObjectPtr<UButtonWidget> Button_Quit = nullptr;
 
-	UPROPERTY()
+	UPROPERTY(BlueprintReadOnly)
 	TObjectPtr<UButtonWidget> ClickedButton = nullptr;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="FinishScreen", meta=(BindWidget))
@@ -74,4 +74,7 @@ protected:
 	void OnTransitionScreenShowed();
 
 	virtual void OnTransitionScreenShowed_Implementation();
+
+	UFUNCTION()
+	void HandleTransitionShow();
 };
